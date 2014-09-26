beb120cff9ec51c122c1ec7c7b3ed06f7c9f5128
Daniel Pope <djpope2365@gmail.com>
2014-09-26 22:20:26 +0100

    Added privacy policy

--- js/youfeed.js
+++ js/youfeed.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
 	$('#helpLink').tooltip();
 	$('#bookmarklet').tooltip();
 	$('#yfinput').tooltip();
+	$('.contactLink').attr('href', 'https://docs.google.com/forms/d/1abSkucu9d7AMH0YJ9LBk2kDPdMZFExXd_xLytOXbvC4/viewform');
 	$('#yfinput').focus();
 });
